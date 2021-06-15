60d3cee67842af0ca85b69958b54e9a647f6f1fd
Detoxic Soul <r4jjin@gmail.com>
2021-06-15 19:18:29 +0530

    3. String operations

---
+++ src/main/kotlin/3_StringOperations.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("couple of string methods available")
+    var q = "Quest"
+    println("words in upper case ${q.toUpperCase()}")
+    println("words in lower case ${q.toLowerCase()}")
+    println("words in reverse case ${q.reversed()}")
+}
