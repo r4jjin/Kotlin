d4e9bcdc8d36bb807e158199b605c6c29a0c2463
Detoxic Soul <r4jjin@gmail.com>
2021-06-20 11:48:46 +0530

    12. Spread operator
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

---
+++ src/main/kotlin/12_SpreadOperator.kt
@@ -0,0 +1,14 @@
+fun printDetails(msg: String, vararg z: String) {
+    println(msg)
+    z.forEach { value ->
+        println("Item $value")
+    }
+
+}
+
+
+fun main() {
+    val msg = "Spread operator * in this calling method send it as a string"
+    val x = arrayOf("1", "1", "1", "1")
+    printDetails(msg, * x)
+}
