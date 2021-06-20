fd702f38fded15722e2925495113fd3bad59f791
Detoxic Soul <r4jjin@gmail.com>
2021-06-20 12:38:03 +0530

    varargs fixes
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

--- src/main/kotlin/11_Varargs.kt
+++ src/main/kotlin/11_Varargs.kt
@@ -1,6 +1,12 @@
 fun main() {
     vararg1("d", "e", "t", "o", "x", "i", "c")
     vararg2("welcome", "pathu", "vaava")
+    val x = intArrayOf(1, 2, 3, 4, 5)
+    vararg3(9, *x, 10, 11)
+}
+
+fun vararg3(vararg x: Int) {
+    println(x)
 }
 
 private fun vararg1(vararg x: String) {
