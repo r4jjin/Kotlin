6d67318e4fd8efa5b0328792e6d44e714e1e4d9f
Detoxic Soul <r4jjin@gmail.com>
2021-06-21 08:50:59 +0530

    14. Extension functions
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

---
+++ src/main/kotlin/14_ExtensionFunctions.kt
@@ -0,0 +1,21 @@
+fun main() {
+    for (x in 2..9) {
+        if (x.isPrime()) {
+            println(x)
+        }
+    }
+
+}
+
+/*adding a method to exsiting class, the value is referenced by this keyword*/
+fun Int.isPrime(): Boolean {
+    var pr = true
+    for (i in 2 until this) {
+        if (this % i == 0) {
+            pr = false
+            break
+        }
+    }
+
+    return pr
+}
