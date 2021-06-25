4bc834b6afaac77fbb60f938a5622da0acc020bb
Detoxic Soul <r4jjin@gmail.com>
2021-06-25 08:56:19 +0530

    ignore
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

---
+++ src/main/kotlin/22_Enums.kt
@@ -0,0 +1,54 @@
+fun main() {
+    /*properties of enum
+    1.name - This property stores the name of the constant.
+    2.ordinal-This property stores the ordinal value of the constant, which is usually a zero-based index.
+    */
+    println(Gender.Female.name)
+    println(Gender.Male.ordinal)
+
+    /*Methods of enum
+    1. values: This method returns a list of all the constants defined within the enum class.
+    2. valueOf: This methods returns the enum constant defined in enum, matching the input string. If the constant, is not present in the enum, then an IllegalArgumentException is thrown.
+    */
+    println(Cards.valueOf(Cards.Spade.toString()))
+    println(Gender.values())
+
+    /*
+    3. Usage of when expression with enum class –
+    */
+    when(DAYS.SUNDAY){
+        DAYS.SUNDAY -> println("Today is Sunday")
+        DAYS.MONDAY -> println("Today is Monday")
+        DAYS.TUESDAY -> println("Today is Tuesday")
+        DAYS.WEDNESDAY -> println("Today is Wednesday")
+        DAYS.THURSDAY -> println("Today is Thursday")
+        DAYS.FRIDAY -> println("Today is Friday")
+        DAYS.SATURDAY -> println("Today is Saturday")
+        // Adding an else clause will generate a warning
+    }
+
+    println(Cards.Spade.s)
+}
+
+/*1. enum constants are instances of an Enum class*/
+enum class Gender {
+    Male,
+    Female,
+    Other
+}
+
+/*2.the constants can be initialized by passing specific values to the primary constructor.*/
+enum class Cards(val s: String) {
+    Spade("black"),
+    Hearts("red")
+}
+
+enum class DAYS{
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY;
+}
