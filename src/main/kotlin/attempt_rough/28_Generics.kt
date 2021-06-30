989f51c38de6370fbf44e8cf8c73df2c6d1453db
Detoxic Spirit <r4jjin@gmail.com>
2021-06-30 08:55:24 +0530

    25. Data class, basic example
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

--- src/main/kotlin/attempt_rough/28_Generics.kt
+++ src/main/kotlin/attempt_rough/28_Generics.kt
@@ -1,5 +1,6 @@
 package attempt_rough
 
+/*
 class Company<T>(text: T) {
     var x = text
 
@@ -11,4 +12,15 @@ class Company<T>(text: T) {
 fun main(args: Array<String>) {
     var name: Company<String> = Company<String>("Basic generics class and method")
     var rank: Company<Int> = Company<Int>(12)
+}
+*/
+
+// star projection in array
+fun printArray(array: Array<*>) {
+    array.forEach { print(it) }
+}
+
+fun main(args: Array<String>) {
+    val name = arrayOf("The", " quick ", "brown", " fox", " jumps", " over", " the", " lazy", " dog")
+    printArray(name)
 }
