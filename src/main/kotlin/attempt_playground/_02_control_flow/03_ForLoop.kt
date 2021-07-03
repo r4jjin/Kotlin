1603d516d262b1f28a34e7252185d39bfc7a3d0d
Detoxic Spirit <r4jjin@gmail.com>
2021-07-03 13:11:34 +0530

    for loop
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

---
+++ src/main/kotlin/attempt_playground/_02_control_flow/03_ForLoop.kt
@@ -0,0 +1,10 @@
+package attempt_playground._02_control_flow
+
+fun main() {
+    val cakes = listOf("carrot", "cheese", "chocolate")
+
+    for (cake in cakes) {                               // 1
+        println("Yummy, it's a $cake cake!")
+    }
+
+}
