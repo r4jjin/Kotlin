d45e136434b74e6ab4ec817b60890781771444f5
Detoxic Spirit <r4jjin@gmail.com>
2021-07-03 13:14:01 +0530

    while and do while constructs
    Signed-off-by: Detoxic Spirit <r4jjin@gmail.com>

---
+++ src/main/kotlin/attempt_playground/_02_control_flow/04_WhileDoWhile.kt
@@ -0,0 +1,20 @@
+package attempt_playground._02_control_flow
+
+fun eatACake() = println("Eat a Cake")
+fun bakeACake() = println("Bake a Cake")
+
+fun main(args: Array<String>) {
+    var cakesEaten = 0
+    var cakesBaked = 0
+
+    while (cakesEaten < 5) {
+        eatACake()
+        cakesEaten++
+    }
+
+    do {
+        bakeACake()
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+
+}
